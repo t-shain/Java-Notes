7abfc61b7cad034c40da697cd64bebde8783f1de
t-shain <97042323+t-shain@users.noreply.github.com>
2023-03-06 11:40:02 -0500

    Create CardTester.java

---
+++ WEEK 8/CardTester.java
@@ -0,0 +1,14 @@
+public class CardTester  {
+	public static void main(String [] args) {
+		Card c1 = new Card(4, Card.HEARTS);
+		Card c2 = new Card(Card.KING, Card.SPADES);
+		
+		Card [] hand = new Card[5]; // hand of five cards //
+		
+		hand[0] = new Card(4, Card.HEARTS);
+		hand[1] = new Card(Card.JACK, Card.SPADES);
+		hand[2] = new Card(5, Card.CLUBS);
+		hand[3] = new Card(
+	}
+}
+  
